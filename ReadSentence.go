36351ecbfc88ffade3ca3122aed4ca4b8cd1ec9c
Ibrahim Oladipupo Qamardeen <direct@kensharval.com>
2020-06-24 07:06:20 +0100

    Progress 200624-0706

---
+++ ReadSentence.go
@@ -0,0 +1,3 @@
+package aaaaaaaa
+
+func ReadSentence () {
---
+++ Sentence.go
@@ -0,0 +1,12 @@
+package aaaaaaaa
+
+type Sentence []byte
+
+func (o *Sentence) add (fig byte) {
+	append (o, fig)
+}
+
+
+func (o *Sentence) removeLast () {}
+func (o *Sentence) clear () {}
+func (o *Sentence) output () {}
